683f60bd9ab21ad4fb5db99e50cbdaa473d2eb98
Syauqi <syauqisnh@gmail.com>
2024-10-20 21:00:32 +0700

    database update

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ const connection = await mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT
 });
 
 
